71c1d906be1394b519655923dcfac91bd1abe7cd
Paula Cavalcante <paula@phpstaff.com.br>
2021-06-18 16:49:19 -0300

    ajuste rota de pesquisa

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,8 @@
 <?php
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AutomovelController;
 
-// Route::apiResource('veiculos', AutomovelController::class);
-
-Route::group(['prefix' => 'v1'], function() {
-    Route::get('veiculos', 'App\Http\Controllers\AutomovelController@index');
-    Route::get('veiculos/${veiculo}', 'App\Http\Controllers\AutomovelController@show');
-    Route::get('veiculos/find', 'App\Http\Controllers\AutomovelController@find'); // rota da busca, que não funciona
-    Route::post('veiculos', 'App\Http\Controllers\AutomovelController@store');
-    Route::put('veiculos/${veiculo}', 'App\Http\Controllers\AutomovelController@update');
-    Route::delete('veiculos/${veiculo}', 'App\Http\Controllers\AutomovelController@destroy');
-});
+Route::apiResource('veiculos', AutomovelController::class);
+Route::get('find', 'App\Http\Controllers\AutomovelController@find');
